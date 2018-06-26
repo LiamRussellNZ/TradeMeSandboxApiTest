e7f6b8a3cc00d31e79b55cf133db653681b6be64
Liam Russell <liam@enuffsaid.co.nz>
2018-06-26 13:06:59 +1200

    Included a ping check just because

--- main.py
+++ main.py
@@ -2,15 +2,30 @@
 #CanRelist = true
 #The Promotions element with Name = "Gallery" has a Description that contains the text "2x larger image"
 
+import os
 import requests
-response=requests.get('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
-print(response)
-data=response.json()
+def endpoint_setup():
+    response=requests.get('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
+    return response
+
+api=endpoint_setup()
+data=api.json()
+print(api)
 
 Promotions_Data= data['Promotions']
 
+def check_ping():
+    hostname = "www.google.co.nz"
+    response = os.system("ping -n 1 " + hostname)
+    if response == 0:
+        pingstatus = "Network Active"
+    else:
+        pingstatus = "Network Error"
+
+    return pingstatus
+
 def Promotions_test(Phrase):
-	print('Testing if the promotions element with Name = "Gallery" has a description that contains the text "2x larger image"')
+	print('Testing if the promotions element with Name = "Gallery" has a description that contains the text "'+Phrase)
 	for element in Promotions_Data:		
 		for k in element.values():
 			if k =='Gallery':
@@ -21,11 +36,21 @@ def Promotions_test(Phrase):
 				else:
 					return False
 
+def test_pingTest():
+	assert check_ping()=="Network Active"
+
+def test_endpointReachable():
+	assert endpoint_setup().status_code==200
+	print('Endpoint is reachable')
+
+
 def test_name():
-	assert data['Name']=='Carbon credits'
+    print('Testing if name is equal to Carbon credits')
+    assert data['Name']=='Carbon credits'
 
 def test_canRelist():
 	assert data['CanRelist']==True
 
 def test_promotionsDescription():
-	assert Promotions_test('Does this work')==True #'2x larger image'
+	assert Promotions_test('2x larger image')==True 
+	
