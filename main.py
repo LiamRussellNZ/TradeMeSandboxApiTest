bba73a5f405f88aa71c6f628d97e90a9b55aac1f
Liam Russell <liam@enuffsaid.co.nz>
2018-07-02 16:42:00 +1200

    Cleaned up main

--- main.py
+++ main.py
@@ -1,56 +1,52 @@
-#Name = "Carbon credits"
-#CanRelist = true
-#The Promotions element with Name = "Gallery" has a Description that contains the text "2x larger image"
+try:
+	import os
+	import requests
 
-import os
-import requests
-def endpoint_setup():
-    response=requests.get('https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false')
-    return response
-
-api=endpoint_setup()
-data=api.json()
-print(api)
-
-Promotions_Data= data['Promotions']
-
-def check_ping():
-    hostname = "www.google.co.nz"
-    response = os.system("ping -n 1 " + hostname)
-    if response == 0:
-        pingstatus = "Network Active"
-    else:
-        pingstatus = "Network Error"
-
-    return pingstatus
-
-def Promotions_test(Phrase):
-	print('Testing if the promotions element with Name = "Gallery" has a description that contains the text "'+Phrase)
-	for element in Promotions_Data:		
-		for k in element.values():
-			if k =='Gallery':
-				elementDescription=element['Description']
-				print('Testing if '+Phrase+' is in description')
-				if Phrase in elementDescription:
-					return True
-				else:
-					return False
-
-def test_pingTest():
-	assert check_ping()=="Network Active"
-
-def test_endpointReachable():
-	assert endpoint_setup().status_code==200
-	print('Endpoint is reachable')
+	url= 'https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false'
 
 
-def test_name():
-    print('Testing if name is equal to Carbon credits')
-    assert data['Name']=='Carbon credits'
+	def endpoint_setup():
+		response=requests.get(url)
+		return response
 
-def test_canRelist():
-	assert data['CanRelist']==True
+	api=endpoint_setup()
+	data=api.json()
 
-def test_promotionsDescription():
-	assert Promotions_test('2x larger image')==True 
-	
+	endpointResponse=endpoint_setup().status_code
+	nameCheck='Carbon credits'
+
+	Promotions_Data= data['Promotions']
+
+	#Promotions has element with Name = "Gallery" has a Description that contains the text "2x larger image"
+	promotionGalleryText='2x larger image'
+
+	canRelistValue=True
+
+	def Promotions_test(Phrase):
+		print('Testing if the promotions element with Name = "Gallery" has a description that contains the text "'+Phrase)
+		for element in Promotions_Data:		
+			for k in element.values():
+				if k =='Gallery':
+					elementDescription=element['Description']
+					if Phrase in elementDescription:
+						return True
+					else:
+						return False
+
+	def test_endpointReachableSends200():
+		assert endpointResponse==200
+		print('Endpoint returns '+str(endpointResponse))
+
+	def test_name():
+		print('Testing if name is equal to '+nameCheck)
+		assert data['Name']==nameCheck
+
+	def test_canRelist():
+		print('Testing if CanRelist element is set to '+str(canRelistValue))
+		assert data['CanRelist']==canRelistValue
+
+	def test_promotionsDescription():
+		assert Promotions_test(promotionGalleryText)==True 
+
+except:
+	print("Some went wrong")
